1735ef4e608cb33b7031f04cf0e948edeb6ba1a1
donald.osgood@phrehab.com <donald.osgood@phrehab.com>
2023-10-28 17:50:30 -0500

    assignement 2 python git hub test

---
+++ hello_world_test.py
@@ -0,0 +1,16 @@
+""" 
+@Program: hello_world_test
+@Author: Donald Osgood
+@Last Date: 2023-10-28 17:45:28
+@Purpose:Donald Osgood
+"""
+
+
+def main():
+    """_summary_
+    """
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
